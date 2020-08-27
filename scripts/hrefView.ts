67e06ded4db04acabfc662bd107ff4eed1351776
avi hadad <avih75@gmail.com>
2020-08-28 00:36:29 +0300

    robust

--- scripts/hrefView.ts
+++ scripts/hrefView.ts
@@ -1,9 +1,11 @@
-// import * as WitService from "TFS/WorkItemTracking/Services";
 import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
 
-export function CreateView(hrefLink: string, FieldRefName: string) {
-    let isView: boolean = false;   // show image state
-    let image = $("#image");      // image element
+let view :boolean=false;
+let fieldRefName:string="";
+
+export function CreateView(hrefLink: string, FieldRefName: string) { 
+    fieldRefName=FieldRefName;
+    let image = $("#image");
     let check = $("#check");      // button switch show state
     check.text("Show");
     let href = $("#href");        // button open new tab with image link
@@ -18,77 +20,66 @@ export function CreateView(hrefLink: string, FieldRefName: string) {
         href.text("");
         href.hide();
         check.hide();
-        OnFieldLeavFocus(textBox.val(), FieldRefName, isView);
-        VSS.resize(null, 40);
-        image.attr("src", "");
-        image.hide();
-    });
-    check.click(() => {
-        isView = !isView
-        AddImageIfexists(hrefLink, isView);
-    })
-    textBox.val(hrefLink);
-    textBox.focusout(() => OnFieldLeavFocus(textBox.val(), FieldRefName, isView));
-    if (hrefLink == undefined || hrefLink == "") {
+        view=false;
         href.hide();
         check.hide();
-        image.hide();
         textBox.show();
-        AddImageIfexists(hrefLink, isView)
-    }
-    else {
-        textBox.hide();
-        check.show();
-        href.show();
-        href.attr("href", hrefLink);
-        href.text(hrefLink.substring(0, 50) + " ...");
-        AddImageIfexists(hrefLink, isView)
-    }
+        image.hide();
+        image.attr("src", "");
+        VSS.resize(null, null);
+        WorkItemFormService.getService().then(
+            (service) => {
+                service.setFieldValue(fieldRefName, hrefLink);
+            }
+        );
+    });
+    check.click(() => {
+        view = !view
+        AddImageIfexists(hrefLink);
+    })
+    textBox.val(hrefLink);
+    textBox.focusout(() => OnFieldLeavFocus());
+    OnFieldLeavFocus( ); 
 }
-function OnFieldLeavFocus(hrefLink: string, FieldRefName: string, isView: boolean) {
+function OnFieldLeavFocus() {    
     let textBox = $("#input");
+    let hrefLink = textBox.val();
     let check = $("#check");
     let href = $("#href");
     let image = $("#image");
+    image.attr("src", hrefLink);
     if (hrefLink == "") {
         href.hide();
         check.hide();
         textBox.show();
         image.hide();
         image.attr("src", "");
-        VSS.resize(null, 40);
+        VSS.resize(null, null);
     }
     else {
         textBox.hide();
         href.attr("href", hrefLink);
-        href.text(hrefLink.substring(0, 50) + " ...");
+        href.text(hrefLink);
         href.show();
         check.show(); 
-        AddImageIfexists(hrefLink, isView);
+        AddImageIfexists(hrefLink);
     }
     WorkItemFormService.getService().then(
         (service) => {
-            service.setFieldValue(FieldRefName, hrefLink);
+            service.setFieldValue(fieldRefName, hrefLink);
         }
     );
 }
-function AddImageIfexists(hrefLink: string, isView: boolean) {
+function AddImageIfexists(hrefLink: string) {
     let image = $("#image");
-    let check = $("#check");
-    image.attr("src", hrefLink);
-    if (isView) {
-        image.show();
-        check.text("Hide");
-        //     $.get(hrefLink)
-        //         .done(function () {
-        VSS.resize(null, 290);
-        //         }).fail(function () {
-        //             VSS.resize(null, 60);
-        //         })
+    let check = $("#check");    
+    if (view) {
+        check.text("Hide"); 
+        image.show(); 
     }
     else {
-        VSS.resize(null, 40);
         check.text("Show");
-        image.hide();
+        image.hide(); 
     }
+    VSS.resize(null, null);
 }
